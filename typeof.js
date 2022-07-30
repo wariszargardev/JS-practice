89aa68330706c3f46347db9ba9e755ed49fd7f2b
wariszargardev <wariszargardev@gmail.com>
2022-07-30 12:05:37 +0500

    typeof

---
+++ typeof.js
@@ -0,0 +1,31 @@
+// In JavaScript there are 5 different data types that can contain values:
+
+// string
+// number
+// boolean
+// object
+// function
+// There are 6 types of objects:
+//
+//     Object
+// Date
+// Array
+// String
+// Number
+// Boolean
+// And 2 data types that cannot contain values:
+//
+//     null
+// undefined
+
+
+typeof "John"                 // Returns "string"
+typeof 3.14                   // Returns "number"
+typeof NaN                    // Returns "number"
+typeof false                  // Returns "boolean"
+typeof [1,2,3,4]              // Returns "object"
+typeof {name:'John', age:34}  // Returns "object"
+typeof new Date()             // Returns "object"
+typeof function () {}         // Returns "function"
+typeof myCar                  // Returns "undefined" *
+typeof null                   // Returns "object"   
