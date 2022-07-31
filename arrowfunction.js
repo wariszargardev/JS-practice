57a357277939e941dd4eb796ac6caa8d2e75f0ec
wariszargardev <wariszargardev@gmail.com>
2022-07-31 11:43:38 +0500

    arrow function

---
+++ arrowfunction.js
@@ -0,0 +1,17 @@
+arrowFunction = () => console.log("Arrow function")
+arrowFunction()
+
+arrowFunction1 = () => {
+    let a = 5;
+    let b = 3;
+    let c = a + b;
+    console.log("Result is " + c)
+}
+
+arrowFunction1()
+
+arrowFunction2 = (n1 , n2) =>{
+    console.log(n1 + n2)
+}
+
+arrowFunction2(10 , 5 )
