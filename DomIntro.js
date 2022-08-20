4afcef71711137e872274e2e724fefb384e2cb77
wariszargardev <wariszargardev@gmail.com>
2022-08-20 09:38:00 +0500

    js-html-dom

---
+++ DomIntro.js
@@ -0,0 +1,10 @@
+// With the HTML DOM, JavaScript can access and change all the elements of an HTML document.
+// The HTML DOM (Document Object Model)
+// In other words: The HTML DOM is a standard for how to get, change, add, or delete HTML elements.
+// JavaScript can change all the HTML elements in the page
+// JavaScript can change all the HTML attributes in the page
+// JavaScript can change all the CSS styles in the page
+// JavaScript can remove existing HTML elements and attributes
+// JavaScript can add new HTML elements and attributes
+// JavaScript can react to all existing HTML events in the page
+// JavaScript can create new HTML events in the page
